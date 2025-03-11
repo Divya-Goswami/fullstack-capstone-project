9c8a2f25f1e5b27717f019940adb6413f031cc46
Divya <divya.goswami1111@gmail.com>
2025-03-12 04:10:41 +0530

    Register API with JWT

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -2,5 +2,7 @@ const config = {
   backendUrl: process.env.REACT_APP_BACKEND_URL,
 };
 
+import {urlConfig} from '../../config';
+
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
 export {config as urlConfig}
