c61dbcd9a1e0b294559cc28845d8373d2390e7b4
Divya <divya.goswami1111@gmail.com>
2025-03-13 02:39:21 +0530

    Fix issue

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -12,8 +12,7 @@ const port = 3060;
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
+});  // <-- semicolon added
 
 
 app.use(express.json());
@@ -46,9 +45,9 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+});  // <-- semicolon added
